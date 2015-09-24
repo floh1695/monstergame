1a38652f15303919531c73843e23c7b39fa31c3e
Charles Bayley <cbayley@puck.uranus.com>
2015-09-23 18:47:04 -0700

    Removed all of the original code base
    * It was a small base but I felt it wasn't structured into the project very well

--- monstergame/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python2
-
-if __name__ == '__main__':
-    print('Running __init__ as __main__')
-
--- monstergame/__main__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python2
-
-if __name__ == '__main__':
-    print('Entering game')
-
