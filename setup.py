2293e0285c92fc95e8772773051b25e990151788
Charles Bayley <cbayley95@gmail.com>
2015-10-06 23:53:22 -0700

    Added a setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='monstergame',
+    version='0.1.1',
+    packages=['monstergame'],
+    url='https://github.com/floh1695/monstergame',
+    license='Apache License',
+    author='Charles Bayley',
+    author_email='cbayley95@gmail.com',
+    description='A simple monster taming/breeding game.'
+)
