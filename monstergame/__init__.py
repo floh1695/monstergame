623771cf56eb704dbdef1f011902e01d1cd5350a
Charles Bayley <cbayley95@gmail.com>
2015-10-06 20:39:25 -0700

    Updated the readme to no longer say anything about Kivy Added bare bones monstergame package

---
+++ monstergame/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python2
+
+if __name__ == '__main__':
+    print 'Nothing is currently importable in this package.'
+    print 'It is currently \'run only\''
+    exit(-1)
+
---
+++ monstergame/__main__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python2
+
+if __name__ == '__main__':
+     print 'Starting game!'
+
